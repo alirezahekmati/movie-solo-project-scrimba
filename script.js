e8f50b107297f1f3da0417b9c1a6e0980ec31258
alireza <alirezahekmati80@gmail.com>
2022-03-01 13:01:22 -0800

    done going to add local storage and then some css

--- script.js
+++ script.js
@@ -1,32 +1,40 @@
 
 
-const conatinerDown = document.querySelector(".container-down")
-const searchButton = document.querySelector(".search-btn")
-const searchInput = document.querySelector(".search input")
-const watchlist  = document.querySelector(".container-up-watchlist-btn")
-const watchlistBtn =document.querySelector(".watch-list-btn")
+let conatinerDown = document.querySelector(".container-down")
+let searchButton = document.querySelector(".search-btn")
+let searchInput = document.querySelector(".search input")
+let watchlist  = document.querySelector(".container-up-watchlist-btn")
+let watchlistBtn =document.querySelector(".watch-list-btn")
 let moviePageNumber =0
 let movieWatchlistArray =[]
-let movieTitle = "Schindler's List"
-searchInput.addEventListener("change",()=>{
+let movieTitle = "aaaaaaaaaaaaaaaa"
+function searchInputFunc(){
     movieTitle  =searchInput.value || "Schindler's List"
-})
+    console.log("boo")
+    conatinerDown = document.querySelector(".container-down")
+searchButton = document.querySelector(".search-btn")
+searchInput = document.querySelector(".search input")
+watchlist  = document.querySelector(".container-up-watchlist-btn")
+watchlistBtn =document.querySelector(".watch-list-btn")
+}
+searchInput.addEventListener("change",searchInputFunc)
 
 searchButton.addEventListener("click",apiCall)
 async function apiCall(){
     const respone = await fetch(`http://www.omdbapi.com/?t=${movieTitle}&apikey=ce4e166a`)
     const data = await respone.json()
-    console.log(data.Response)
+    console.log(data)
 
-if (data.Response===true){
-    let title = data.Title    
+if (data.Response =="True"){
+    console.log("boo2")
+let title = data.Title    
 let poster = data.Poster
 let plot = data.Plot
 let runtime = data.Runtime
 let rating = data.Ratings[0].Value
 let genre = data.Genre
     conatinerDown.innerHTML = `
-    <div class="container-down-movie  movie-${moviePageNumber}">
+    <div class="container-down-movie movie--${moviePageNumber}">
         <div class="container-down-movie-right">
             <img src=${poster}>
         </div>
@@ -44,7 +52,7 @@ let genre = data.Genre
                 <p>
                     ${genre}
                 </p>
-                <button class="watch-list-btn" onclick="addWatchlist()">
+                <button class="watch-list-btn" onclick="addWatchlist(event)">
                     <!-- add a plus icon  -->
                     Watchlist 
                 </button>
@@ -56,12 +64,19 @@ let genre = data.Genre
     </div>`
     moviePageNumber++
 }
-else{
+else  
+{
     conatinerDown.innerHTML = `<h2>unable to fint what you're looking for.please try another search</h2>`
 }
+conatinerDown = document.querySelector(".container-down")
+searchButton = document.querySelector(".search-btn")
+searchInput = document.querySelector(".search input")
+watchlist  = document.querySelector(".container-up-watchlist-btn")
+watchlistBtn =document.querySelector(".watch-list-btn")
 
 }
-watchlist.addEventListener("click",function(){
+function watchlistFunc(){
+    conatinerDown.innerHTML =""
     if(!movieWatchlistArray.length){
         document.querySelector(".container").innerHTML =`
         <div class="container">
@@ -77,43 +92,86 @@ watchlist.addEventListener("click",function(){
         </div>   
             `
     }
-    conatinerDown.innerHTML =""
-    conatinerDown.innerHTML +=movieWatchlistArray.map(each=> {
+    else{
+        document.querySelector(".search").remove()
+        document.querySelector(".container-up").outerHTML = `
+            <div class="container-up">
+                <h1>My  watchlist</h1>
+                <h3 class="container-up-watchlist-btn"  onclick="returnHomePage()">Search for a movie !</h3>
+                
+            </div>`
+        conatinerDown.innerHTML +=movieWatchlistArray.map(each=> {
         
-        return each.outerHTML
-    }).join("")
-    
-    
-    for(let i=0 ;i<moviePageNumber;i++ ){
+            return each.outerHTML
+        }).join("")
         
-        document.querySelector(`.movie-${i} .watch-list-btn`).outerHTML =`<button class="watch-list-btn" onclick="removeFromList(event)">remove</button>`
+       
+        for(let i=0 ;i<moviePageNumber;i++ ){
+            
+            document.querySelector(`.movie--${i} .watch-list-btn`).outerHTML =`<button class="watch-list-btn" onclick="removeFromList(event)">remove</button>`
+        }
     }
+    conatinerDown = document.querySelector(".container-down")
+searchButton = document.querySelector(".search-btn")
+searchInput = document.querySelector(".search input")
+watchlist  = document.querySelector(".container-up-watchlist-btn")
+watchlistBtn =document.querySelector(".watch-list-btn")
+}
+watchlist.addEventListener("click",watchlistFunc)
+function addWatchlist(event){
     
-})
-function addWatchlist(){
-    
-    movieWatchlistArray.unshift(document.querySelector(`.movie-${moviePageNumber-1}`))
-    
+    movieWatchlistArray.unshift(document.querySelector(`.movie--${moviePageNumber-1}`))
+    event.currentTarget.textContent ="added"
+    conatinerDown = document.querySelector(".container-down")
+searchButton = document.querySelector(".search-btn")
+searchInput = document.querySelector(".search input")
+watchlist  = document.querySelector(".container-up-watchlist-btn")
+watchlistBtn =document.querySelector(".watch-list-btn")
     
 }
 function removeFromList(event){
+    let arry
+    arry=event.currentTarget.parentElement.parentElement.parentElement.classList[1]
+    
+    movieWatchlistArray.map(each => {
+        if(each.classList[1]==arry){
+            
+            
+            console.log(movieWatchlistArray)
+            movieWatchlistArray.splice(movieWatchlistArray.indexOf(each),movieWatchlistArray.indexOf(each)+1)
+            console.log(movieWatchlistArray)
+            moviePageNumber--
+        }
+    })
+
     
     event.currentTarget.parentElement.parentElement.parentElement.remove()
+    conatinerDown = document.querySelector(".container-down")
+searchButton = document.querySelector(".search-btn")
+searchInput = document.querySelector(".search input")
+watchlist  = document.querySelector(".container-up-watchlist-btn")
+watchlistBtn =document.querySelector(".watch-list-btn")
+    
 }
 function returnHomePage(){
     document.querySelector(".container").innerHTML =`
     <div class="container-up">
         <h1>Find your film</h1>
-        <h3 class="container-up-watchlist-btn">My watchlist</h3>
+        <h3 class="container-up-watchlist-btn"  onclick="watchlistFunc()">My watchlist</h3>
         
     </div>
     <div class="search">
-        <input type="text" name="" id="" placeholder="search for a movie title">
-        <button class="search-btn">search</button>
+        <input type="text" name="" id="" placeholder="search for a movie title"  onchange="searchInputFunc()">
+        <button class="search-btn"  onclick="apiCall()">search</button>
     </div>
     <div class="container-down">
         
         <h3 class="explore">start exploring</h3>
     </div>
 `
+conatinerDown = document.querySelector(".container-down")
+searchButton = document.querySelector(".search-btn")
+searchInput = document.querySelector(".search input")
+watchlist  = document.querySelector(".container-up-watchlist-btn")
+watchlistBtn =document.querySelector(".watch-list-btn")
 }
