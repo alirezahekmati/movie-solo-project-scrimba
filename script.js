07b2c01e4d6df06df5def5e971c945491a57497b
alireza <alirezahekmati80@gmail.com>
2022-03-01 18:26:29 -0800

    done v1

--- script.js
+++ script.js
@@ -6,27 +6,39 @@ let searchInput = document.querySelector(".search input")
 let watchlist  = document.querySelector(".container-up-watchlist-btn")
 let watchlistBtn =document.querySelector(".watch-list-btn")
 let moviePageNumber =0
-let movieWatchlistArray =[]
+let movieWatchlistArray = JSON.parse(localStorage.getItem("key")) || []
 let movieTitle = "aaaaaaaaaaaaaaaa"
+
+
+//  neet to save movieWatchlistArray in local storage  so the past watchlist will exsist 
+//  how to do it ?
+//      localStorage.setItem()
+//      localStorage.getItem()
+//      localStorage.setItem("key",JSON.stringify())
+//      JSON.parse(localStorage.getItem("key"))
+//  what to consider?
+//      should update localstorage  if a movie has been removed
+//      should update localstorage  if a movie has been added
+//      should give localstorage data when about to render watchlist page
+//  is there  somting missing ?
+//  define when you are going to pass data to storage or get data from storage 
+//pass data 
+//get data 
+// maybe insted of whole  html  we only save the movie object on it ?
 function searchInputFunc(){
     movieTitle  =searchInput.value || "Schindler's List"
-    console.log("boo")
     conatinerDown = document.querySelector(".container-down")
-searchButton = document.querySelector(".search-btn")
-searchInput = document.querySelector(".search input")
-watchlist  = document.querySelector(".container-up-watchlist-btn")
-watchlistBtn =document.querySelector(".watch-list-btn")
+    searchButton = document.querySelector(".search-btn")
+    searchInput = document.querySelector(".search input")
+    watchlist  = document.querySelector(".container-up-watchlist-btn")
+    watchlistBtn =document.querySelector(".watch-list-btn")
 }
 searchInput.addEventListener("change",searchInputFunc)
-
 searchButton.addEventListener("click",apiCall)
 async function apiCall(){
     const respone = await fetch(`http://www.omdbapi.com/?t=${movieTitle}&apikey=ce4e166a`)
     const data = await respone.json()
-    console.log(data)
-
 if (data.Response =="True"){
-    console.log("boo2")
 let title = data.Title    
 let poster = data.Poster
 let plot = data.Plot
@@ -68,15 +80,16 @@ else
 {
     conatinerDown.innerHTML = `<h2>unable to fint what you're looking for.please try another search</h2>`
 }
-conatinerDown = document.querySelector(".container-down")
-searchButton = document.querySelector(".search-btn")
-searchInput = document.querySelector(".search input")
-watchlist  = document.querySelector(".container-up-watchlist-btn")
-watchlistBtn =document.querySelector(".watch-list-btn")
-
+    conatinerDown = document.querySelector(".container-down")
+    searchButton = document.querySelector(".search-btn")
+    searchInput = document.querySelector(".search input")
+    watchlist  = document.querySelector(".container-up-watchlist-btn")
+    watchlistBtn =document.querySelector(".watch-list-btn")
 }
 function watchlistFunc(){
     conatinerDown.innerHTML =""
+
+        //get data 
     if(!movieWatchlistArray.length){
         document.querySelector(".container").innerHTML =`
         <div class="container">
@@ -98,60 +111,60 @@ function watchlistFunc(){
             <div class="container-up">
                 <h1>My  watchlist</h1>
                 <h3 class="container-up-watchlist-btn"  onclick="returnHomePage()">Search for a movie !</h3>
-                
             </div>`
-        conatinerDown.innerHTML +=movieWatchlistArray.map(each=> {
-        
-            return each.outerHTML
+
+            //get data 
+        conatinerDown.innerHTML +=JSON.parse(localStorage.getItem("key")).map(each=> {
+            return each
         }).join("")
-        
-       
-        for(let i=0 ;i<moviePageNumber;i++ ){
-            
+        for(let i=0 ;i<JSON.parse(localStorage.getItem("key")).length;i++ ){
             document.querySelector(`.movie--${i} .watch-list-btn`).outerHTML =`<button class="watch-list-btn" onclick="removeFromList(event)">remove</button>`
         }
     }
     conatinerDown = document.querySelector(".container-down")
-searchButton = document.querySelector(".search-btn")
-searchInput = document.querySelector(".search input")
-watchlist  = document.querySelector(".container-up-watchlist-btn")
-watchlistBtn =document.querySelector(".watch-list-btn")
+    searchButton = document.querySelector(".search-btn")
+    searchInput = document.querySelector(".search input")
+    watchlist  = document.querySelector(".container-up-watchlist-btn")
+    watchlistBtn =document.querySelector(".watch-list-btn")
 }
 watchlist.addEventListener("click",watchlistFunc)
 function addWatchlist(event){
+    //pass data  new  data added going to save it 
+
+    movieWatchlistArray.unshift(document.querySelector(`.movie--${moviePageNumber-1}`).outerHTML)
+    localStorage.setItem("key",JSON.stringify(movieWatchlistArray))
     
-    movieWatchlistArray.unshift(document.querySelector(`.movie--${moviePageNumber-1}`))
+    
     event.currentTarget.textContent ="added"
     conatinerDown = document.querySelector(".container-down")
-searchButton = document.querySelector(".search-btn")
-searchInput = document.querySelector(".search input")
-watchlist  = document.querySelector(".container-up-watchlist-btn")
-watchlistBtn =document.querySelector(".watch-list-btn")
-    
+    searchButton = document.querySelector(".search-btn")
+    searchInput = document.querySelector(".search input")
+    watchlist  = document.querySelector(".container-up-watchlist-btn")
+    watchlistBtn =document.querySelector(".watch-list-btn")
 }
 function removeFromList(event){
     let arry
     arry=event.currentTarget.parentElement.parentElement.parentElement.classList[1]
-    
+
+//get data 
     movieWatchlistArray.map(each => {
-        if(each.classList[1]==arry){
-            
-            
-            console.log(movieWatchlistArray)
+        if(each.includes(arry)){
+        
+//get data 
+//pass data   remove the item  that are been removed from  movieWatchlistArray  from local storage
+
             movieWatchlistArray.splice(movieWatchlistArray.indexOf(each),movieWatchlistArray.indexOf(each)+1)
-            console.log(movieWatchlistArray)
+            localStorage.setItem("key",JSON.stringify(movieWatchlistArray))
             moviePageNumber--
         }
     })
-
-    
+    console.log(JSON.parse(localStorage.getItem("key")))
     event.currentTarget.parentElement.parentElement.parentElement.remove()
     conatinerDown = document.querySelector(".container-down")
-searchButton = document.querySelector(".search-btn")
-searchInput = document.querySelector(".search input")
-watchlist  = document.querySelector(".container-up-watchlist-btn")
-watchlistBtn =document.querySelector(".watch-list-btn")
-    
+    searchButton = document.querySelector(".search-btn")
+    searchInput = document.querySelector(".search input")
+    watchlist  = document.querySelector(".container-up-watchlist-btn")
+    watchlistBtn =document.querySelector(".watch-list-btn")
 }
 function returnHomePage(){
     document.querySelector(".container").innerHTML =`
